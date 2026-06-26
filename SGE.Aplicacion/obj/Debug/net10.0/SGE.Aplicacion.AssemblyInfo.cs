50a40a19814bc5932f470adf82e7672322d549d4
JuanBlasGonzalez <jbgs2003@gmail.com>
2026-06-26 00:22:40 -0300

    modificaciones

--- SGE.Aplicacion/obj/Debug/net10.0/SGE.Aplicacion.AssemblyInfo.cs
+++ SGE.Aplicacion/obj/Debug/net10.0/SGE.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61206e4941660ca1efd09d72198772531ded78c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c19733705d500e974f49177d43d11609fd8cca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
